1dbefb481252182af021d4300538263b8d00adca
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-08-05 10:42:54 +0300

    better search

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from base64 import encode
-import pandas as pd
 import requests
 import time
 import json
-
-from tqdm import tqdm
-from itertools import product
 
 
 def get(word):
@@ -21,47 +16,72 @@ if __name__ == '__main__':
     from queue import PriorityQueue
 
     model = Word2Vec.load('model.mdl')
+
     seen = set()
     q = PriorityQueue()
-    INIT_SIMILARITY = -50
-    q.put((INIT_SIMILARITY, 'ילד'))
-    q.put((INIT_SIMILARITY, 'בגד'))
-    q.put((INIT_SIMILARITY, 'חומר'))
-    q.put((INIT_SIMILARITY, 'נוזל'))
-    q.put((INIT_SIMILARITY, 'תפקיד'))
-    q.put((INIT_SIMILARITY, 'מכשיר'))
-    q.put((INIT_SIMILARITY, 'מקרר'))
-    q.put((INIT_SIMILARITY, 'ילדה'))
-    q.put((INIT_SIMILARITY, 'רגש'))
-    q.put((INIT_SIMILARITY, 'תחושה'))
-    q.put((INIT_SIMILARITY, 'חצאית'))
-    q.put((INIT_SIMILARITY, 'שיער'))
-    q.put((INIT_SIMILARITY, 'פנים'))
-    q.put((INIT_SIMILARITY, 'מטוס'))
-    q.put((INIT_SIMILARITY, 'מנוע'))
+    INIT_SIMILARITY = -950
 
+    def init_q(words):
+        for word in words:
+            seen.add(word)
+            q.put((INIT_SIMILARITY, word))
+
+    init_q([
+        'אמבטיה',
+        'בגד',
+        'בובה',
+        'חדר',
+        'חומר',
+        'חצאית',
+        'ילד',
+        'ילדה',
+        'מחשב',
+        'מיטה',
+        'מטוס',
+        'מכשיר',
+        'מנוע',
+        'מקרר',
+        'נוזל',
+        'ספר',
+        'עיניים',
+        'עצמי',
+        'פנים',
+        'פעולה',
+        'קציר',
+        'רגש',
+        'רהיט',
+        'שעון',
+        'שיער',
+        'תחושה',
+        'תפקיד',
+    ])
+
+    best_distance = -1
+    words = []
     with open('words.json', 'w', encoding='utf-8') as f:
         while q:
             p, word = q.get()
-            seen.add(word)
-
             r = get(word)
 
-            similarity = r['similarity']
-            if not similarity:
+            if not r['similarity']:
                 continue
 
             print(json.dumps(r), file=f)
 
             distance = r['distance']
-            if distance > 700:
-                print(f'[{q.qsize()}]', distance, r['word'])
+            best_distance = max(best_distance, distance)
+
+            if distance == 1_000:
+                break
+
+            words.append((distance, word))
+            print(sorted(words)[-10:])
 
             topn = max(4, distance)
             topn = int(topn ** 0.5)
             for similar, _ in model.wv.most_similar(word, topn=topn):
                 if similar not in seen:
-                    q.put((-similarity, similar))
+                    q.put((-distance, similar))
                     seen.add(similar)
 
             time.sleep(1)
