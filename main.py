0616711d77a1445b7db538665183f55be22ce066
Gilad Kutiel <gilad.kutiel@gmail.com>
2023-12-26 20:59:30 +0200

    fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import dis
 import heapq as hq
 import json
 import os
@@ -25,7 +26,7 @@ if __name__ == '__main__':
 
     seen = set()
     q = []
-    best_distance = 0
+    best_distance = -2
     best_word = None
     bar = tqdm(unit='it', desc='Searching')
     date = dt.strftime(dt.now(), '%Y-%m-%d')
@@ -48,24 +49,25 @@ if __name__ == '__main__':
                 finally:
                     time.sleep(1)
 
-            with open('seed.txt', encoding='utf8') as f:
-                for word in tqdm(f.read().splitlines(), desc='Loading seed'):
+            def add_all(words, desc):
+                global best_distance, best_word
+                words = tqdm(words, desc=desc)
+                for word in words:
                     dist = add(word)
                     if dist > best_distance:
                         best_distance = dist
                         best_word = word
 
-            while q and best_distance < 1_000:
+                    words.set_postfix(distance=best_distance, word=best_word)
+                    if dist == 1000:
+                        break
+
+            with open('seed.txt', encoding='utf8') as f:
+                add_all(f.read().splitlines(), desc='Loading seed')
+
+            def words():
                 _, word = hq.heappop(q)
-
                 for similar, _ in model.wv.most_similar(word, topn=30):
-                    dist = add(similar)
-                    if dist > best_distance:
-                        best_distance = dist
-                        best_word = similar
+                    yield similar
 
-                bar.set_postfix(
-                    distance=best_distance,
-                    word=best_word)
-
-                bar.update()
+            add_all(words(), desc='Searching')
